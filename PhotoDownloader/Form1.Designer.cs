f5eb707b19d5b2cdca8aefcef1d4ba0490c50d54
lodomur <gmstudio1916@gmail.com>
2016-11-24 20:53:24 +0100

    Test version 1.0.0

---
+++ PhotoDownloader/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace PhotoDownloader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnDownload = new System.Windows.Forms.Button();
+            this.fieldInput = new System.Windows.Forms.TextBox();
+            this.dropDownImageList = new System.Windows.Forms.ComboBox();
+            this.btnToDo = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbUrlSite = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDownload.Location = new System.Drawing.Point(432, 407);
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(419, 40);
+            this.btnDownload.TabIndex = 0;
+            this.btnDownload.Text = "&Показати вибране зображення";
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // fieldInput
+            // 
+            this.fieldInput.Location = new System.Drawing.Point(12, 110);
+            this.fieldInput.Multiline = true;
+            this.fieldInput.Name = "fieldInput";
+            this.fieldInput.Size = new System.Drawing.Size(419, 291);
+            this.fieldInput.TabIndex = 1;
+            // 
+            // dropDownImageList
+            // 
+            this.dropDownImageList.FormattingEnabled = true;
+            this.dropDownImageList.Location = new System.Drawing.Point(12, 70);
+            this.dropDownImageList.Name = "dropDownImageList";
+            this.dropDownImageList.Size = new System.Drawing.Size(839, 21);
+            this.dropDownImageList.TabIndex = 2;
+            // 
+            // btnToDo
+            // 
+            this.btnToDo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnToDo.Location = new System.Drawing.Point(12, 407);
+            this.btnToDo.Name = "btnToDo";
+            this.btnToDo.Size = new System.Drawing.Size(419, 40);
+            this.btnToDo.TabIndex = 3;
+            this.btnToDo.Text = "&Обробити дані";
+            this.btnToDo.UseVisualStyleBackColor = true;
+            this.btnToDo.Click += new System.EventHandler(this.btnToDo_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox.Image")));
+            this.pictureBox.Location = new System.Drawing.Point(432, 110);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(419, 290);
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox.TabIndex = 4;
+            this.pictureBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Випадаючий список фотографій";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Код веб-сторінки";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(429, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Картинка";
+            // 
+            // tbUrlSite
+            // 
+            this.tbUrlSite.Location = new System.Drawing.Point(12, 31);
+            this.tbUrlSite.Name = "tbUrlSite";
+            this.tbUrlSite.Size = new System.Drawing.Size(839, 20);
+            this.tbUrlSite.TabIndex = 8;
+            this.tbUrlSite.Text = "http://humus.livejournal.com/4218852.html";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Адреса сайту:";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(863, 459);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.tbUrlSite);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.btnToDo);
+            this.Controls.Add(this.dropDownImageList);
+            this.Controls.Add(this.fieldInput);
+            this.Controls.Add(this.btnDownload);
+            this.Name = "MainForm";
+            this.Text = "Завантажувач фото з веб-сторінок";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.TextBox fieldInput;
+        private System.Windows.Forms.ComboBox dropDownImageList;
+        private System.Windows.Forms.Button btnToDo;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbUrlSite;
+        private System.Windows.Forms.Label label4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ PhotoDownloader/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using HtmlAgilityPack;
+
+namespace PhotoDownloader
+{
+    public partial class MainForm : Form
+    {
+
+     
+        
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnToDo_Click(object sender, EventArgs e)
+        {
+            System.Net.WebClient wc = new System.Net.WebClient();
+            string webData = wc.DownloadString(tbUrlSite.Text);
+            fieldInput.ScrollBars = ScrollBars.Vertical;
+            fieldInput.Text = webData;
+
+
+            List<string> listImg = new List<string>();
+          
+            HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+            doc.LoadHtml(webData);
+            foreach (HtmlNode img in doc.DocumentNode.SelectNodes("//img"))
+            {
+                
+                listImg.Add(img.GetAttributeValue("src", null));
+                
+            }
+
+            dropDownImageList.DataSource = listImg.ToList();
+
+
+            try
+            {
+                List<string> pcPath = new List<string>();
+                for (int counter = 6; counter < (listImg.Select(x => x)).Count(); counter++)
+                {
+                    WebClient webClient = new WebClient();
+                    webClient.DownloadFile(listImg.ElementAt(counter), "D://image" + counter + ".png");
+                    pcPath.Add("D://image" + counter + ".png");
+                }
+            }
+            catch (Exception exception)
+            {
+
+                MessageBox.Show("Деякі файли можуть бути не збережені на Ваг комп'ютер. Під час роботи програми виникла помилка веб-сторінки!");
+            }
+
+           
+           
+    
+            
+
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+
+         
+            pictureBox.ImageLocation = dropDownImageList.Text;
+        }
+    }
+}
